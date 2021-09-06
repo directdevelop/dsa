7d805bcff81123e686380d9b144c4ee943246eb1
Amitabh <amitabh@Amitabhs-MacBook-Pro.local>
2021-09-06 21:20:29 +0530

    added 2D array example

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
     public static void main(String[] A) {
-        System.out.printf("dsaproject");
-        System.out.printf("Amitabh Sarkar");
+//        System.out.printf("dsaproject");
+//        System.out.printf("Amitabh Sarkar");
+
+
+//        int[][] arr = new int[5][];
+        int[][] arr = {
+                {1, 2, 3, 4, 6},
+                {5, 6, 7, 8},
+                {10, 20, 30, 40},
+
+        };
+        int[] id = {1, 2, 3, 4, 5};
+
+        System.out.println("arr2D " + (arr[2][3]));
+
+        for (int row = 0; row < arr.length; row++) {
+            System.out.println("-----row-----" + row);
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.println("col " + col);
+                System.out.println(arr[row][col]);
+            }
+        }
+
+
     }
 }

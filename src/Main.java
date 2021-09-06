17409135048bb93ff0f88403d35e6fc78cf6bb6a
Amitabh <amitabh@Amitabhs-MacBook-Pro.local>
2021-09-06 20:47:43 +0530

    added sout

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] A){
+    public static void main(String[] A) {
         System.out.printf("dsaproject");
+        System.out.printf("Amitabh Sarkar");
     }
 }
